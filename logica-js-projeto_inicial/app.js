9f7ebddf68a4e7bfd9ef7a8710ea94b4a9c4b2e5
Gustavo Rocha <gurocha05@gmail.com>
2024-11-18 17:34:14 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,20 +1,30 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 console.log(`O número secreto é ${numeroSecreto}`);
 let chute;
+let tentativas = 1;
 
 while(chute != numeroSecreto){
     chute = prompt('Escolha um númeor entre 1 e 10');
 
     if(chute == numeroSecreto) {
-        alert(`Você acertou o número secreto ${numeroSecreto} !`);   
+         break;
     } else{
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else{
             alert(`O número secreto é maior que ${chute}`);
         }
-    }
-           
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }  
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa} !`);  
+
+// if(tentativas > 1) {
+//     alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas !`);  
+// } else{
+//     alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa !`);  
+// } 
