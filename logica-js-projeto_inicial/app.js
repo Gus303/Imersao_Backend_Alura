4b019c01253d4d3ae404f441f1b3c5543b832c96
Gustavo Rocha <gurocha05@gmail.com>
2024-11-18 16:43:06 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
-alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(`O número secreto é ${numeroSecreto}`);
+alert('Boas vindas ao jogo do número secreto');
 let chute = prompt('Escolha um númeor entre 1 e 10');
 
 if(chute == numeroSecreto) {
-    console.log('Você acertou o número secreto (5) !');   
+    alert(`Você acertou o número secreto ${numeroSecreto} !`);   
+} else{
+    alert('Você errou');
 }
