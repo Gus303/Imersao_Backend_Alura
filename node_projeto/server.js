4608c6260e4be7d7dd7692ed6dcc897207bcbb0c
Gustavo Rocha <gurocha05@gmail.com>
2024-11-19 15:46:21 -0300

    Update server.js

--- node_projeto/server.js
+++ node_projeto/server.js
@@ -1,11 +1,25 @@
 import express from 'express';
 
+const post = [ {descricao: 'foto teste 1', 
+                    imagem: 'https://placecats.com/millie/300/150' },
+                {descricao: 'foto teste 2', 
+                    imagem: 'https://placecats.com/millie/300/150' },
+                {descricao: 'foto teste 3', 
+                    imagem: 'https://placecats.com/millie/300/150' },
+                {descricao: 'foto teste 4', 
+                    imagem: 'https://placecats.com/millie/300/150' },
+                {descricao: 'foto teste 5', 
+                    imagem: 'https://placecats.com/millie/300/150' },
+                {descricao: 'foto teste 6', 
+                    imagem: 'https://placecats.com/millie/300/150' } ];
+
 const app = express();
+app.use(express.json())
 app.listen(3000, () => {
     console.log('Sevidor escutando...');
     
 });
 
-app.get('/api', (rec, res) => {
-    res.status(200).send('Bem vindo');
+app.get('/posts', (rec, res) => {
+    res.status(200).json(post);
 });
