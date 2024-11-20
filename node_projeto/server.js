893d4f46a0e8ba3055d9f901d039c237c21ba13f
Gustavo Rocha <gurocha05@gmail.com>
2024-11-20 13:03:05 -0300

    criacao do src

--- node_projeto/server.js
+++ node_projeto/server.js
@@ -1,35 +1,11 @@
 import express from 'express';
-import conectarAoBanco from '../src/config/dbConfig.js';
-
-// Conecta ao banco de dados usando a string de conexão fornecida como variável de ambiente.
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+import routes from '../src/routes/postRoutes.js';
 
 // Cria uma instância do servidor Express.
 const app = express();
-
-// Habilita o parsing de JSON no corpo das requisições.
-app.use(express.json());
+routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console.
 app.listen(3000, () => {
   console.log('Servidor escutando...');
 });
-
-// Função assíncrona para obter todos os posts do banco de dados.
-async function getTodosPosts() {
-  // Seleciona o banco de dados "imersao-back-end".
-  const db = conexao.db("imersao-back-end");
-  // Seleciona a coleção "posts".
-  const colecao = db.collection("posts");
-  // Retorna um array com todos os documentos da coleção.
-  return colecao.find().toArray();
-}
-
-// Rota para obter todos os posts.
-app.get("/posts", async (req, res) => {
-  // Chama a função para obter os posts do banco de dados.
-  const posts = await getTodosPosts();
-  // Envia os posts como resposta em formato JSON com status 200 (sucesso).
-  res.status(200).json(posts);
-});
- 
