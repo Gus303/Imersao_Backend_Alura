69f98e6dcc8a0977006e79273525ddf6b4fbc332
Gustavo Rocha <gurocha05@gmail.com>
2024-11-21 14:04:08 -0300

    rotas

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,12 @@
-import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import express from 'express';
+import { listarPosts, postarNovoPost } from '../controllers/postsController.js';
 const routes = (app) => {
     // Habilita o parsing de JSON no corpo das requisições.
 app.use(express.json());
     // Rota para obter todos os posts.
-app.get("/posts", listarPosts);
+app.get('/posts', listarPosts);
+app.post('/post', postarNovoPost)
+
 }
 
 export default routes;
