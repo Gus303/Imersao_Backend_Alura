1ddff8ad6a8cfd8c8976ee25203934d519ea48b7
Gustavo Rocha <gurocha05@gmail.com>
2024-11-22 15:35:42 -0300

    multer

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     // Habilita o parsing de JSON no corpo das requisições.
@@ -7,7 +19,7 @@ app.use(express.json());
     // Rota para obter todos os posts.
 app.get("/posts", listarPosts);
 app.post("/posts", postarNovoPost);
-
+app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
