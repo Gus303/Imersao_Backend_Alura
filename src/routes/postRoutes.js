1ec51307f3c73756d88a61c8681b74bba8146cef
Gustavo Rocha <gurocha05@gmail.com>
2024-11-24 20:47:56 -0300

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin:"http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,6 +22,7 @@ const upload = multer({ dest: "./uploads" , storage});
 const routes = (app) => {
     // Habilita o parsing de JSON no corpo das requisições.
 app.use(express.json());
+app.use(cors(corsOptions));
     // Rota para obter todos os posts.
 app.get("/posts", listarPosts);
 app.post("/posts", postarNovoPost);
